d6fe760b397c4a73fd59f52c58f1ea7418e8c086
Alexei Zaikin <zaikinaleks04@mail.ru>
2025-12-17 17:20:13 +0300

    style header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import '../assets/styles/header.scss'
+import '../assets/styles/main.scss'
 import Logotype from '../assets/media/icons/logo_xp.jpeg'
 import SunIcon from '../assets/media/icons/sun.svg'
 import LuneIcon from '../assets/media/icons/lune.svg'
@@ -6,12 +7,12 @@ import LuneIcon from '../assets/media/icons/lune.svg'
 export default function Header () {
     return (
         <header className="header">
-            <div className="container">
+            <div className="container df jcsb aic">
                 <a className="header__logotype" href="./index.html">
                     <img className="header__logo" src={Logotype} alt="A&S"/>
                 </a>
                 <nav className="header__nav">
-                    <ul className="header__list">
+                    <ul className="header__list df aic g20">
                         <li className="header__item"><a href="#main" className="header__link">Привет</a></li>
                         <li className="header__item"><a href="#about" className="header__link">Обо мне</a></li>
                         <li className="header__item"><a href="#services" className="header__link">Услуги</a></li>
@@ -20,9 +21,9 @@ export default function Header () {
                         <li className="header__item"><a href="#contacts" className="header__link">Контакты</a></li>
                     </ul>
                 </nav>
-                <div className="header__control">
+                <div className="header__control df aic g20">
                     <button className="header__themeToggle">
-                        <img className="header__icon" src={LuneIcon} alt="Светлая тема"/>
+                        <img className="header__icon" src={SunIcon} alt="Светлая тема"/>
                     </button>
                     <select className="header__selectLang" name="select">
                         <option className="header__optionLang" value="russian" selected>RU</option>
