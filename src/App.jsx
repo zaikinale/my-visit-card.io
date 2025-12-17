4957275d221f752ca79ed1b86aab75e3f7528b1b
Alexei Zaikin <zaikinaleks04@mail.ru>
2025-12-17 18:28:10 +0300

    added block hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header.jsx'
+import Hero from './components/Hero.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
         <Header />
+        <Hero />
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import '../assets/styles/main.scss'
+import '../assets/styles/hero.scss'
+import MyImg from '../../public/me.jpeg'
+
+
+export default  function Hero() {
+    return (
+        <main className="hero">
+            <h2 className="hero__title"></h2>
+            <div className="container df aci jcc g20">
+                <img src={MyImg} alt="Фото начинающего разработчика Заикина Алексея Сергеевича" className="hero__img"/>
+                <div className="hero__content df fdc g10 block">
+                    <h2 className="hero__subtitle">Заикин Алексей Сергеевич <br/> <strong>Frontend-разработчик из Санкт-Петербурга</strong></h2>
+
+                    <p className="hero__text">Делаю быстрые, адаптивные и легко поддерживаемые сайты на React + TypeScript</p>
+                    <a href="https://t.me/@A7S_list" className="hero__btn">Написать в Telegram ↗</a>
+                </div>
+            </div>
+        </main>
+    )
+}
