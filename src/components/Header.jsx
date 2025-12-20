3e4ce770653dbf0ae58dfed29681f73208eaf522
Alexei Zaikin <zaikinaleks04@mail.ru>
2025-12-20 17:54:12 +0300

    added adaptive header and some fix components styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,27 @@
-import '../assets/styles/header.scss'
-import '../assets/styles/main.scss'
-import Logotype from '../assets/media/icons/logo_xp.jpeg'
-import SunIcon from '../assets/media/icons/sun.svg'
-import LuneIcon from '../assets/media/icons/lune.svg'
-import MenuIcon from '../assets/media/icons/menu.svg'
-import {useState} from "react";
+// Header.jsx
+import '../assets/styles/header.scss';
+import '../assets/styles/main.scss';
+import Logotype from '../assets/media/icons/logo_xp.jpeg';
+import SunIcon from '../assets/media/icons/sun.svg';
+import LuneIcon from '../assets/media/icons/lune.svg';
+import MenuIcon from '../assets/media/icons/menu.svg';
+import { useState } from "react";
 
-export default function Header () {
-    const [isShow, setIsShow] = useState(false)
-    function handleShow () {
-        setIsShow(r => !r)
+export default function Header() {
+    const [isShow, setIsShow] = useState(false);
+
+    function handleShow() {
+        setIsShow(r => !r);
     }
+
     return (
         <header className="header">
             <div className="container df jcsb aic fww">
                 <a className="header__logotype df aic g10" href="./index.html">
-                    <img className="header__logo" src={Logotype} alt="A&S"/>
+                    <img className="header__logo" src={Logotype} alt="A&S" />
                     <h2 className="header__title">A&S</h2>
                 </a>
+
                 <nav className="header__nav">
                     <ul className="header__list df aic g20 fww">
                         <li className="header__item"><a href="#main" className="header__link link">Привет</a></li>
@@ -28,13 +32,23 @@ export default function Header () {
                         <li className="header__item"><a href="#contacts" className="header__link link">Контакты</a></li>
                     </ul>
                 </nav>
+
                 <div className="header__control df aic g20 fww">
-                    {/*<button className="header__themeToggle">*/}
-                    {/*    <img className="header__icon" src={SunIcon} alt="Светлая тема"/>*/}
-                    {/*</button>*/}
-                    <button className="header__menu" onClick={handleShow}>
-                        <img src={MenuIcon} alt="Кнопка меню" className="header__icon"/>
+                    <button className="header__btn" onClick={handleShow}>
+                        {isShow ? 'Закрыть' : <img src={MenuIcon} alt="Кнопка меню" className="header__icon" />}
                     </button>
+
+                    {isShow && (
+                        <ul className="header__menu fdc g20 fww block">
+                            <li className="header__item"><a onClick={handleShow} href="#main" className="header__link link">Привет</a></li>
+                            <li className="header__item"><a onClick={handleShow} href="#about" className="header__link link">Обо мне</a></li>
+                            <li className="header__item"><a onClick={handleShow} href="#services" className="header__link link">Услуги</a></li>
+                            <li className="header__item"><a onClick={handleShow} href="#tech" className="header__link link">Технологии</a></li>
+                            <li className="header__item"><a onClick={handleShow} href="#portfolio" className="header__link link">Портфолио</a></li>
+                            <li className="header__item"><a onClick={handleShow} href="#contacts" className="header__link link">Контакты</a></li>
+                        </ul>
+                    )}
+
                     <select className="header__selectLang" name="select">
                         <option className="header__optionLang" value="russian" selected>RU</option>
                         <option className="header__optionLang" value="english">EN</option>
@@ -42,5 +56,5 @@ export default function Header () {
                 </div>
             </div>
         </header>
-    )
+    );
 }
